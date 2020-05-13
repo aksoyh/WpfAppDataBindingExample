157116f2a8c758e73e21d2ab5e0a52519b470979
aksoy <aksoy.android@gmail.com>
2020-05-13 23:18:45 +0300

    initial commit

--- WpfAppDataBindingExample/MainWindow.xaml.cs
+++ WpfAppDataBindingExample/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +26,42 @@ namespace WpfAppDataBindingExample
         {
             InitializeComponent();
         }
+
+        ObservableCollection<Person> persons = new ObservableCollection<Person>();
+        Person prs;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Add için 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            prs = new Person();
+            grid1.DataContext = prs;
+        }
+
+        // Save için
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            // persons koleksiyonuna yeni bir <Person> nesnesi ekler.
+            persons.Add(prs);
+            lstBox.ItemsSource = persons;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            // Resim dosyası seçilerek <Person> nesnesinin img özelliğine set edilir.
+            OpenFileDialog file = new OpenFileDialog();
+            file.ShowDialog();
+            if(file.FileName != null)
+            {
+                prs.Img = file.FileName;
+            }
+        }
+
+        // MVVM = Model - View - ViewModel
+        // DataBinding = Binding
     }
 }
---
+++ WpfAppDataBindingExample/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppDataBindingExample
+{
+    class Person : INotifyPropertyChanged
+    {
+        int _id;
+        string _name;
+        string _img;
+        
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public string Img {
+            get
+            {
+                return _img;
+            }
+            set
+            {
+                _img = value;
+                propertyChanged("img");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void propertyChanged(string prop)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
